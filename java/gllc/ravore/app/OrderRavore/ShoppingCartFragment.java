0ccd4726d327fb70d0eae69cd317ba1ed33ab1b2
sonubee <notifications3972@gmail.com>
2016-03-31 20:49:35 -0700

    Revert "Next Revert"
    This reverts commit 2832abf306ba62ba2bf89620aeb394cb5a89e253.

--- java/gllc/ravore/app/OrderRavore/ShoppingCartFragment.java
+++ java/gllc/ravore/app/OrderRavore/ShoppingCartFragment.java
@@ -27,7 +27,7 @@ public class ShoppingCartFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.actual_order_screen, container, false);
-        Log.i("MyActivity", "FIIIIIIIIIIIIIIIIIINDMEEEE");
+
         return view;
     }
 
