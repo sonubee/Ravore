27ec0a6f0bcb7abec6890d69394c966a44688b8a
sonubee <notifications3972@gmail.com>
2016-03-31 20:47:51 -0700

    Revert "Revert "Revert "Testing"""
    This reverts commit 991338dbfa9f8fc200d935063d5f033b85caff7c.

--- java/gllc/ravore/app/OrderRavore/ShoppingCartFragment.java
+++ java/gllc/ravore/app/OrderRavore/ShoppingCartFragment.java
@@ -29,7 +29,6 @@ public class ShoppingCartFragment extends Fragment {
         View view = inflater.inflate(R.layout.actual_order_screen, container, false);
         Log.i("MyActivity", "FIIIIIIIIIIIIIIIIIINDMEEEE");
         Log.i("MyActivity", "FIIIIIIIIIIIIIIIIIINDMEEEE");
-        Log.i("MyActivity", "FIIIIIIIIIIIIIIIIIINDMEEEE");
         return view;
     }
 
