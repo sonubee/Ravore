35a43bfb5f809963c6264934eeb3b0b2ca95435d
sonubee <notifications3972@gmail.com>
2016-05-05 22:15:58 -0700

    With Pointer

--- java/gllc/ravore/app/FestivalInfo/MapFragment.java
+++ java/gllc/ravore/app/FestivalInfo/MapFragment.java
@@ -6,6 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapView;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 import gllc.ravore.app.R;
 
@@ -13,6 +25,9 @@ import gllc.ravore.app.R;
  * Created by bhangoo on 5/3/2016.
  */
 public class MapFragment extends Fragment {
+
+    MapView mMapView;
+    private GoogleMap googleMap;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -22,11 +37,51 @@ public class MapFragment extends Fragment {
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
+/*
         View view = inflater.inflate(R.layout.frame_layout_festival_info, container, false);
 
-        return view;
+        //SupportMapFragment mapFragment = (SupportMapFragment) getActivity().getSupportFragmentManager()
+        //        .findFragmentById(R.id.fragment_container_festival_info);
+        //mapFragment.getMapAsync(this);
 
+        return view;
+*/
+        // inflat and return the layout
+        View v = inflater.inflate(R.layout.fragment_location_info, container,
+                false);
+        mMapView = (MapView) v.findViewById(R.id.mapView);
+        mMapView.onCreate(savedInstanceState);
+
+        mMapView.onResume();// needed to get the map to display immediately
+
+        try {
+            MapsInitializer.initialize(getActivity().getApplicationContext());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        googleMap = mMapView.getMap();
+        // latitude and longitude
+        double latitude = 17.385044;
+        double longitude = 78.486671;
+
+        // create marker
+        MarkerOptions marker = new MarkerOptions().position(
+                new LatLng(latitude, longitude)).title("Hello Maps");
+
+        // Changing marker icon
+        marker.icon(BitmapDescriptorFactory
+                .defaultMarker(BitmapDescriptorFactory.HUE_ROSE));
+
+        // adding marker
+        googleMap.addMarker(marker);
+        CameraPosition cameraPosition = new CameraPosition.Builder()
+                .target(new LatLng(17.385044, 78.486671)).zoom(12).build();
+        googleMap.animateCamera(CameraUpdateFactory
+                .newCameraPosition(cameraPosition));
+
+        // Perform any camera updates here
+        return v;
 
     }
 
@@ -36,4 +91,6 @@ public class MapFragment extends Fragment {
 
 
     }
+
+
 }
